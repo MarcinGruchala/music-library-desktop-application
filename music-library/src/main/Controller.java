b34052da5b8e17419151b7ac99f49421144595c8
Marcin Gruchała <margruchala@gmail.com>
2021-05-25 14:29:20 +0200

    Connected to database

--- music-library/src/main/Controller.java
+++ music-library/src/main/Controller.java
@@ -1,4 +1,18 @@
 package main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Controller {
+    Connection connection;
+
+    public void getConnection(){
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection("jdbc:oracle:thin:@//192.168.0.180:1521/XEPDB1","hr","hr");
+            System.out.println("Connected to Oracle database server");
+        }catch (Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
 }
--- music-library/src/main/Main.java
+++ music-library/src/main/Main.java
@@ -8,12 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Controller controller = new Controller();
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("albums_scene.fxml"));
         primaryStage.setTitle("Music library");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        controller.getConnection();
     }
 
 
