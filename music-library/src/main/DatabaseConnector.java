22912f88fe46eb4d81119950ed71450d959bb67d
Marcin Gruchała <margruchala@gmail.com>
2021-05-30 12:32:48 +0200

    Added JDBC_URL to swap

--- music-library/src/main/DatabaseConnector.java
+++ music-library/src/main/DatabaseConnector.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public  class DatabaseConnector {
-    private final static String JDBC_URL = "jdbc:oracle:thin:@//192.168.0.153:1521/XEPDB1";
+    //private final static String JDBC_URL = "jdbc:oracle:thin:@//192.168.0.153:1521/XEPDB1"; //Piotra
+    private final static String JDBC_URL = "jdbc:oracle:thin:@//192.168.0.180:1521/XEPDB1"; //Marcina
     private final static String JDBC_USERNAME = "hr";
     private final static String JDBC_PASSWORD = "hr";
 
@@ -13,7 +14,7 @@ public  class DatabaseConnector {
     public static Connection getConnection() {
         return connection;
     }
-
+    
     static public void setConnection(){
         try {
             connection = DriverManager.getConnection(JDBC_URL,JDBC_USERNAME,JDBC_PASSWORD);
